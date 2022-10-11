802fb25771836626eaf5c4caccd3c3a298bed6fe
Jonathan Martins <jcmartin@ualberta.ca>
2022-10-11 12:22:48 -0600

    Complete assignment 2

--- httpclient.py
+++ httpclient.py
@@ -27,15 +27,22 @@ import urllib.parse
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
+
+def get_remote_ip(host):
+    try:
+        remote_ip = socket.gethostbyname( host )
+    except socket.gaierror:
+        print ('Hostname could not be resolved. Exiting')
+        sys.exit()
+    return remote_ip
+
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
-
-    def connect(self, host, port):
+    def connect(self, host, port=80):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
@@ -55,6 +62,13 @@ class HTTPClient(object):
     def close(self):
         self.socket.close()
 
+    def create_connection(self, url):
+        parsed_url = urllib.parse.urlparse(url)
+        self.host = parsed_url.hostname
+        self.port = parsed_url.port if parsed_url.port else 80
+        self.path = parsed_url.path if parsed_url.path else "/"
+        self.connect(self.host, self.port)
+
     # read everything from the socket
     def recvall(self, sock):
         buffer = bytearray()
@@ -67,15 +81,40 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    def create_get_request_header(self):
+        return f"GET {self.path} HTTP/1.1\r\n" \
+            f"Host: {self.host}\r\n" \
+            f"Connection: Close\r\n\r\n"
+
+    def create_post_request_header(self):
+        return f"POST {self.path} HTTP/1.1\r\n" \
+            f"Host: {self.host}\r\n" \
+            f"Content-Type: application/x-www-form-urlencoded\r\n" \
+            f"Content-Length: {len(self.content)}\r\n" \
+            f"Connection: Close\r\n\r\n" \
+            f"{self.content}\r\n"
+
+    def send_and_process_response(self, get):
+        if get:
+            self.sendall(self.create_get_request_header())
+        else:
+            self.sendall(self.create_post_request_header())
+        server_response = self.recvall(self.socket)
+        self.close();
+        self.code = int(server_response.split()[1])
+        self.body = server_response.split("\r\n\r\n")[1]
+        print(server_response)
+
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        self.create_connection(url)      
+        self.send_and_process_response(1)  
+        return HTTPResponse(self.code, self.body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        self.create_connection(url)
+        self.content = urllib.parse.urlencode(args) if args else ''
+        self.send_and_process_response(0)  
+        return HTTPResponse(self.code, self.body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
@@ -93,3 +132,4 @@ if __name__ == "__main__":
         print(client.command( sys.argv[2], sys.argv[1] ))
     else:
         print(client.command( sys.argv[1] ))
+
